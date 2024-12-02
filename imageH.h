db9f26b3d3b484764dbc7d2c5f73bdb0dee11f17
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2024-12-02 07:41:59 +0600

    Add files via upload

---
+++ imageH.h
@@ -0,0 +1,21 @@
+#ifndef __IMAGE_H_
+#define __IMAGE_H_
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure for storing the image data
+typedef struct PGMImage {
+	char pgmType[3];
+	unsigned char** data;
+	unsigned int width;
+	unsigned int height;
+	unsigned int maxColorValue;
+} PGMImage;
+
+#define COLOR_RANGE 255
+
+#endif
