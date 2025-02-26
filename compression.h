d94fa91d54e51089432a69a86022d16644113c9b
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2025-02-26 22:06:23 +0600

    Update compression.h

--- compression.h
+++ compression.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <jpeglib.h>
 
 // For BMP image compression
 #include "combined_bmp_huffman.c"
@@ -11,15 +12,10 @@
 
 // For PGM image compression
 
-// P2 Header
-#include "p2Huff.c"
-#include "p2Run.c"
-#include "p2LZW.c"
+#include "pgmHuff.c"
+#include "pgmRun.c"
+#include "pgmLZW.c"
 
-// P5 Header
-#include "p5Huff.c"
-#include "p5Run.c"
-#include "p5LZW.c"
 
 // For JPEG image compression
-
+#include "JpegDCT.c"
